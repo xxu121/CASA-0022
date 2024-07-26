8efd313cc4bd9bd4a6d9afd76025b4a87587dd39
xxu121 <146341729+xxu121@users.noreply.github.com>
2024-07-26 17:06:46 +0100

    Update DS18B20.ino

--- DS18B20.ino
+++ DS18B20.ino
@@ -5,11 +5,9 @@
 #include <DallasTemperature.h>
 
 // DS18B20 Sensor
-#define ONE_WIRE_BUS 2 // Data wire is connected to pin 2
+#define ONE_WIRE_BUS 2
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-
-float Temperature;
 
 // WiFi and MQTT credentials
 #include "arduino_secrets.h" 
@@ -28,47 +26,40 @@ PubSubClient client(espClient);
 Timezone GB;
 
 // Global variables
-long lastMsg = 0;
+float Temperature;
 char msg[50];
-unsigned long lastPublish = 0; // Variable to store the last publish time
-const long publishInterval = 5000; // Publish interval in milliseconds (5 seconds)
-
-// Function declarations
-void startWifi();
-void syncDate();
-void sendMQTT();
-void readTemperature();
-void callback(char* topic, byte* payload, unsigned int length);
-void reconnect();
 
 void setup() {
   Serial.begin(115200);
-
-  // Initialize DS18B20 sensor
   sensors.begin();
-
-  // Initialize WiFi
+  checkWiFiModule();
   startWifi();
-
-  // Sync date and time
   syncDate();
-
-  // Start MQTT server
   client.setServer(mqtt_server, 1884);
   client.setCallback(callback);
 }
 
 void loop() {
-  // Read and send temperature data periodically
+  if (WiFi.status() != WL_CONNECTED) {
+    startWifi();
+  }
+
   readTemperature();
   sendMQTT();
-  
-  // Print current time
-  Serial.println(GB.dateTime("H:i:s"));
-  
-  // Handle MQTT client loop
   client.loop();
+  Serial.println(GB.dateTime("H:i:s"));
   delay(5000);
+}
+
+void checkWiFiModule() {
+  if (WiFi.status() == WL_NO_MODULE) {
+    Serial.println("Communication with WiFi module failed!");
+    while (true);
+  }
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
+    Serial.println("Please upgrade the firmware");
+  }
 }
 
 void startWifi() {
@@ -77,32 +68,32 @@ void startWifi() {
   Serial.println(ssid);
   WiFi.begin(ssid, password);
 
-  // Check to see if connected and wait until connected
+  int counter = 0;
   while (WiFi.status() != WL_CONNECTED) {
-    delay(2000);
+    delay(600);
     Serial.print(".");
+    counter++;
+    if (counter > 50) {
+      Serial.println("Resetting due to failed WiFi connection...");
+      NVIC_SystemReset();
+    }
   }
-  
-  Serial.println("");
-  Serial.println("WiFi connected");
+  Serial.println("\nWiFi connected");
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
 }
 
 void syncDate() {
-  // Get real date and time
   waitForSync();
-  Serial.println("UTC: " + UTC.dateTime());
   GB.setLocation("Europe/London");
-  Serial.println("London time: " + GB.dateTime());
+  Serial.println("Time synchronized: " + GB.dateTime());
 }
 
 void readTemperature() {
-  sensors.requestTemperatures(); // Send the command to get temperatures
-  Temperature = sensors.getTempCByIndex(0); // Get the value of the temperature in Celsius
+  sensors.requestTemperatures();
+  Temperature = sensors.getTempCByIndex(0);
   if (Temperature == DEVICE_DISCONNECTED_C) {
     Serial.println("Error: Could not read temperature data");
-    Temperature = NAN; // Set the temperature to NaN (Not a Number) if reading fails
   } else {
     Serial.print("Current Temperature: ");
     Serial.println(Temperature);
@@ -113,47 +104,32 @@ void sendMQTT() {
   if (!client.connected()) {
     reconnect();
   }
-  
-  client.loop();
-  
-  // Always publish the temperature, even if it's NaN
   snprintf(msg, 50, "%.1f", Temperature);
+  client.publish("student/CASA0022/ucfnxxu/freezer temperature 3", msg);
   Serial.print("Publish message for t: ");
   Serial.println(msg);
-  client.publish("student/CASA0022/ucfnxxu/freezer temperature 3", msg);
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
-  
   for (int i = 0; i < length; i++) {
     Serial.print((char)payload[i]);
   }
-  
   Serial.println();
 }
 
 void reconnect() {
-  // Loop until reconnected
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
-    
-    // Create a random client ID
-    String clientId = "mkr1010Client-";
-    clientId += String(random(0xffff), HEX);
-    
-    // Attempt to connect with clientID, username, and password
+    String clientId = "mkr1010Client-" + String(random(0xffff), HEX);
     if (client.connect(clientId.c_str(), mqttuser, mqttpass)) {
       Serial.println("connected");
-      // Resubscribe
       client.subscribe("student/CASA0022/ucfnxxu/inTopic");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
       delay(5000);
     }
   }
